99bf55c7679ecfc8766349939d427a9538ada67f
Emmanuel Wangila <79250258+emmanuelwangila@users.noreply.github.com>
2024-01-27 10:32:51 +0300

    code refactors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Acomplishments from "../components/Acomplishments/Acomplishments";
-import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation";
+// import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation";
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
@@ -12,7 +12,7 @@ const Home = () => {
     <Layout>
       <Section grid>
         <Hero />
-        <BgAnimation />
+        {/* <BgAnimation /> */}
       </Section>
       <Projects />
       <Technologies />
